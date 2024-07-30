f9bf204aac3d784e9fdc830215c2fcc0d5ba8ed5
lainh.dev <lainh.dev@gmail.com>
2024-07-30 11:30:51 +0700

    -

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Lai Blog',
+  author: 'Lai Nguyen',
+  headerTitle: 'LaiBlog',
+  description: 'Insights from an experienced senior full stack engineer who loves to develop applications from passion.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
